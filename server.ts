0b515cd93db691395a1aec4172ec940454b5b077
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-10-09 18:11:14 +0900

    feat: deno KV

--- server.ts
+++ server.ts
@@ -2,6 +2,12 @@ import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 import { viewEngine, etaEngine, oakAdapter } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts"
 import  logger  from "https://deno.land/x/oak_logger/mod.ts";
 
+
+const kv = await Deno.openKv();
+const key = ["users", crypto.randomUUID()];
+const value = { name: "John", created: new Date() };
+await kv.set(key, value);
+
 const app = new Application();
 
 app.use(logger.logger);
@@ -22,6 +28,12 @@ router.get("/", (ctx) => {
 router.get("/home", (ctx) => {
   ctx.render("home.eta", { name: "home" } );
 });
+
+router.get("/data", async(ctx) => {
+  const result = await kv.get(key);
+  console.log(result);
+  ctx.response.body = result;
+})
 
 app.use(router.routes());
 app.use(router.allowedMethods());
